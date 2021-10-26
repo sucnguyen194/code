7837662be2e3ec0f2f92ff438a94e2d4cf3c41c4
sucnguyen194 <58970838+sucnguyen194@users.noreply.github.com>
2021-10-26 15:41:24 +0700

    2

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,15 +2,9 @@
 
 namespace App\Models;
 
-use App\Enums\ProductSessionType;
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\File;
 use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
